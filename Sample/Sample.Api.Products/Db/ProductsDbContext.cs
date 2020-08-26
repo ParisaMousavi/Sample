a3f131d91a9152320239698ff1811e3d1d46d675
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-08-26 17:02:16 +0200

    start product sample project

---
+++ Sample/Sample.Api.Products/Db/ProductsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Products.Db
+{
+    //DbContext is from Microsoft.EntityFrameworkCore
+    public class ProductsDbContext: DbContext 
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
