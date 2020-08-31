6bee9edc670d4b355f7d1c254990f5e3e3529f4f
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2020-08-31 20:15:43 +0200

    add catalog microservice to solution

---
+++ Sample/Sample.Api.Catalog/Db/CatalogsDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Catalog.Db
+{
+    public class CatalogsDbContext : DbContext
+    {
+        public DbSet<Db.Catalog> Catalogs { get; set; }
+        public DbSet<Db.CatalogType> catalogTypes { get; set; }
+
+        public DbSet<Db.CatalogBrand> catalogBrands { get; set; }
+
+        public CatalogsDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // this method must be used to add seeding step
+        }
+    }
+}
