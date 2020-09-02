bdd8e22494974b5953f025bbff0446a4b6c6072a
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-09-02 22:33:01 +0200

    add changes

---
+++ Sample/Sample.Api.Images/Models/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Images.Models
+{
+    public class Messages
+    {
+        public Guid ProductId { get; set; }
+
+        public string ContainerName { get; set; }
+
+        public string ImageName { get; set; }
+
+    }
+}
