8421808f67163c8856907d19e9b8ea0563c0ae4c
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-09-22 08:40:46 +0200

    sample template for only rg

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=2.20.0"
+  features {}
+}
+
+terraform{
+    backend "azurerm" {
+        resource_group_name = "sample-terraform-rg"
+        storage_account_name = "sampleterraform"
+        container_name = "terrform"
+        key = "development.terraform.tfstate"
+    }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+    name = "sample-rg"
+    location = "west us"
+}
