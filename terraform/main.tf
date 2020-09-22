8df2ce13dedcdf260adf40d2849f8e843d11f37c
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-09-22 14:58:48 +0200

    add storage to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,5 +17,23 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
     name = "sample-rg"
-    location = "west us"
+    location = "west europa"
+
+    tags = {
+        environment = "staging",
+        project = "sample"
+    }
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "sampleimagestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging",
+    project = "sample"
+  }
 }
