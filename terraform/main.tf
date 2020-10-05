9a95341502fe6fb440e6490423458e6aa30650d7
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-10-05 12:40:35 +0200

    add acr to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,16 @@ resource "azurerm_storage_account" "storage" {
     project = "sample"
   }
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "azuresampleacr"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Standard"
+  admin_enabled            = true
+
+  tags = {
+    environment = "staging",
+    project = "sample"
+  }
+}
