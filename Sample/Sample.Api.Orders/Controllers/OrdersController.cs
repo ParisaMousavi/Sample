93dc00e5736390a40a65da9fa149ddf488b1a05b
Parisa Moosavinezhad <p.moosavinezhad@tekaris.com>
2020-10-09 23:28:02 +0200

    add controller for order

---
+++ Sample/Sample.Api.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Api.Orders.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Orders.Controllers
+{
+    [ApiController]
+    [Route("api/orders") ]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersProvider _ordersProvider;
+
+        public OrdersController(Interfaces.IOrdersProvider ordersProvider)
+        {
+            this._ordersProvider = ordersProvider;
+        }
+
+        public async Task<IActionResult> GetOrdersAsync()
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> GetOrderAsync()
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> AddOrderAsync()
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> UpdateOrderAsync()
+        {
+            return NotFound();
+        }
+
+        public async Task<IActionResult> DeleteOrderAsync()
+        {
+            return NotFound();
+        }
+
+    }
+}
